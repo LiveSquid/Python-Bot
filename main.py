d53115bab02f359dd44e73838bafc02167993409
LiveSquid <liamashok@gmail.com>
2024-12-02 14:08:37 -0500

    Works Now

--- main.py
+++ main.py
@@ -1,1 +1,50 @@
-print("helloWorld")
+import keyboard
+import time, sys
+import pyautogui
+
+time.sleep(3)
+while True:
+
+    pyautogui.press('D')
+    time.sleep(0.1)
+    pyautogui.press('o')
+    time.sleep(0.1)
+    pyautogui.press('u')
+    time.sleep(0.1)
+    pyautogui.press('b')
+    time.sleep(0.1)
+    pyautogui.press('l')
+    time.sleep(0.1)
+    pyautogui.press('o')
+    time.sleep(0.1)
+    pyautogui.press('o')
+    time.sleep(0.1)
+    pyautogui.press('n')
+    time.sleep(0.1)
+    pyautogui.press('s')
+
+
+
+    time.sleep(5)
+
+    pyautogui.press('backspace')
+    time.sleep(0.1)
+    pyautogui.press('backspace')
+    time.sleep(0.1)
+    pyautogui.press('backspace')
+    time.sleep(0.1)
+    pyautogui.press('backspace')
+    time.sleep(0.1)
+    pyautogui.press('backspace')
+    time.sleep(0.1)
+    pyautogui.press('backspace')
+    time.sleep(0.1)
+    pyautogui.press('backspace')
+    time.sleep(0.1)
+    pyautogui.press('backspace')
+    time.sleep(0.1)
+    pyautogui.press('backspace')
+
+
+    time.sleep(10)
+
